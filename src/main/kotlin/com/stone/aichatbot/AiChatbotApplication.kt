79cfc3542864247c812de375dcb20e558b0ca858
stone <stone@moneybrain.ai>
2025-04-20 14:12:18 +0900

    spring init

---
+++ src/main/kotlin/com/stone/aichatbot/AiChatbotApplication.kt
@@ -0,0 +1,11 @@
+package com.stone.aichatbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiChatbotApplication
+
+fun main(args: Array<String>) {
+    runApplication<AiChatbotApplication>(*args)
+}
